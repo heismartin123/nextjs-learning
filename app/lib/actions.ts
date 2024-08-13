732eb655c2b4f476d2d24920f233e32642825b55
Martin He <martin_he@intuit.com>
2024-08-13 15:44:30 -0700

    feat: chapt 14

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -38,7 +38,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
   });
   // If form validation fails, return errors early. Otherwise, continue.
   if (!validatedFields.success) {
-    console.log(validatedFields.error, 'validatedFields');
+    // console.log(validatedFields.error, "validatedFields");
     return {
       errors: validatedFields.error.flatten().fieldErrors,
       message: "Missing Fields. Failed to Create Invoice.",
@@ -65,12 +65,28 @@ export async function createInvoice(prevState: State, formData: FormData) {
   redirect("/dashboard/invoices");
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    // console.log(validatedFields.error, "updateInvoice flatten validatedFields");
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Update Invoice.",
+    };
+  }
+
+  // const { customerId, amount, status } = UpdateInvoice.parse({
+  //   customerId: formData.get("customerId"),
+  //   amount: formData.get("amount"),
+  //   status: formData.get("status"),
+  // });
+  const { customerId, amount, status } = validatedFields.data;
 
   const amountInCents = amount * 100;
 
